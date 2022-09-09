20698d4b04e21b12e77849147b0b131f25b0f94f
Jacob Bodenbender <jbodenbender10@gmail.com>
2022-09-09 12:54:42 -0400

    finished product

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 8081;
 
-const knex = require('knex')(require('../knexfile.js')['development'])
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV||'development'])
 
 app.get('/', (req, res) => {
     res.send('Application up and running.')
 });
 
 app.get('/pets', (req, res) => {
-    knex('pet')
+    knex
         .select('*')
+        .from('pet')
         .then(data => {
             var petNames = data.map(pet => pet.name)
-            response.json(petNames);
+            res.json(petNames);
         });
 });
 app.listen(port, () => {
